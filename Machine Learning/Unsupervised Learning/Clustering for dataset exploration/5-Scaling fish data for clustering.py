6f1c61f15ec03eb68f1f3cf60181e25d442712f9
aliiz <aliizadi2030@gmail.com>
2018-01-27 23:34:52 +0330

    Clustering for dataset exploration

---
+++ Machine Learning/Unsupervised Learning/Clustering for dataset exploration/5-Scaling fish data for clustering.py
@@ -0,0 +1,14 @@
+# Perform the necessary imports
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+# Create scaler: scaler
+scaler = StandardScaler()
+
+# Create KMeans instance: kmeans
+kmeans = KMeans(n_clusters=4)
+
+# Create pipeline: pipeline
+pipeline = make_pipeline(scaler, kmeans)
+

712c33c6d23d9185f48090f569e87d0173db9f38
aliiz <aliizadi2030@gmail.com>
2018-02-06 23:42:07 +0330

    Exploring the raw data

---
+++ Machine Learning/Machine Learning with the Experts: School Budgets/Exploring the raw data/4-Computing log loss with NumPy.py
@@ -0,0 +1,20 @@
+# Compute and print log loss for 1st case
+correct_confident = compute_log_loss(correct_confident, actual_labels)
+print("Log loss, correct and confident: {}".format(correct_confident)) 
+
+# Compute log loss for 2nd case
+correct_not_confident = compute_log_loss(correct_not_confident, actual_labels)
+print("Log loss, correct and not confident: {}".format(correct_not_confident)) 
+
+# Compute and print log loss for 3rd case
+wrong_not_confident = compute_log_loss(wrong_not_confident, actual_labels)
+print("Log loss, wrong and not confident: {}".format(wrong_not_confident)) 
+
+# Compute and print log loss for 4th case
+wrong_confident = compute_log_loss(wrong_confident, actual_labels)
+print("Log loss, wrong and confident: {}".format(wrong_confident)) 
+
+# Compute and print log loss for actual labels
+actual_labels = compute_log_loss(actual_labels, actual_labels)
+print("Log loss, actual labels: {}".format(actual_labels)) 
+
